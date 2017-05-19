ab956cbf63aba6ab983c73f02da892c80eb4d028
yankedev <yankedev@exteso.com>
2017-05-19 10:59:03 +0200

    Removing GlobalMethodSecurity config to recread the branch to test Step1

--- resource-server/src/main/java/com/exteso/blog/oauth2/stepbystep/ResourceServer.java
+++ resource-server/src/main/java/com/exteso/blog/oauth2/stepbystep/ResourceServer.java
@@ -16,7 +16,6 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableResourceServer
 public class ResourceServer {
 
@@ -26,19 +25,16 @@ public class ResourceServer {
 
     private String message = "Hello world!";
 
-    @PreAuthorize("hasRole('ROLE_RS_READ')")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public Map<String, String> home() {
         return Collections.singletonMap("message", message);
     }
 
-    @PreAuthorize("hasRole('ROLE_RS_WRITE')")
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public void updateMessage(@RequestBody String message) {
         this.message = message;
     }
 
-    @PreAuthorize("#oauth2.hasScope('resource-server-read')")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public Map<String, String> user(Principal user) {
         return Collections.singletonMap("message", "user is: " + user.toString());
